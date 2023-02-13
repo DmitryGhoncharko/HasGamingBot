e885492b86bb5b13247b8ce4960892ab1f0c1acc
dangeonMaster <dafae722@gmail.com>
2023-02-13 15:21:45 +0300

    fix: update bot

--- src/main/java/org/example/GamingBot.java
+++ src/main/java/org/example/GamingBot.java
@@ -1,13 +1,18 @@
 package org.example;
-
 import lombok.SneakyThrows;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.hibernate.query.Query;
+import org.telegram.telegrambots.bots.DefaultAbsSender;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.MessageEntity;
@@ -25,7 +30,7 @@ import javax.swing.*;
 import java.sql.Connection;
 import java.util.*;
 
-public class GamingBot extends TelegramLongPollingBot {
+public class GamingBot extends TelegramLongPollingBot{
 
     private final String BOT_NAME = "BrsmGamingBot";
     private final String BOT_TOKEN = "6221029068:AAF8GY1STrEYMuIG6bO3CgwAQ69ox9rnErk";
@@ -35,6 +40,7 @@ public class GamingBot extends TelegramLongPollingBot {
     private List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
 
     private static String actionAnswer;
+    private static boolean sendGolos = true;
     private final String START_MESSAGE = "▫️Привет,это PREDICTIONS BOT \uD83E\uDD16 \n" +
             "▫️Делай предсказания на матч и за верно угаданный исход получай поинты \uD83E\uDE99\n" +
             "▫️За каждое верное предсказание получай 250\uD83E\uDE99\n" +
@@ -51,7 +57,7 @@ public class GamingBot extends TelegramLongPollingBot {
             Arrays.asList("5964846649")
     );
 
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException{
         GamingBot bot = new GamingBot();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(bot);
@@ -71,31 +77,41 @@ public class GamingBot extends TelegramLongPollingBot {
     @SneakyThrows
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
-        if (update.hasCallbackQuery()) {
+        if(update.hasCallbackQuery()){
             handingButton(update);
             Message messageUpdate = update.getCallbackQuery().getMessage();
-        } else if (adminChatIdList.contains(update.getMessage().getChatId().toString()) && isNumeric(message.getText())) {
+        } else if(adminChatIdList.contains(update.getMessage().getChatId().toString()) && isNumeric(message.getText())){
             /*
             ДОБАВИТЬ ЗАЩИТУ ОТ ГОШАНА
              */
-            if (message.getText().equals("1")) {
-                actionAnswer = "ORIGINAL";
-                addPoints(update);
-            } else if (message.getText().equals("2")) {
-                actionAnswer = "TARGET";
-                addPoints(update);
-            } else {
-                execute(SendMessage.builder().chatId(update.getMessage().getChatId()).text("ГОША ЗАЕБАЛв ").build());
-            }
-            actionAnswer = null;
-        } else if (adminChatIdList.contains(update.getMessage().getChatId().toString())) {
+                if(message.getText().equals("1")){
+                    actionAnswer = "ORIGINAL";
+                    addPoints(update);
+                    sendGolos = true;
+                    actionAnswer = null;
+                }
+                else if (message.getText().equals("2")){
+                    actionAnswer = "TARGET";
+                    addPoints(update);
+                    sendGolos = true;
+                    actionAnswer = null;
+                }else if (message.getText().equals("3")){
+                    sendGolos = false;
+                    List<Person> list = findAllPeopleWithCriteria();
+                    for (Person person : list){
+                        execute(SendMessage.builder().chatId(person.getChat_id()).text("Ставочки на матч больше не принимаются!").build());
+                    }
+                }
+
+
+        } else if(adminChatIdList.contains(update.getMessage().getChatId().toString())){
             createAdminPost(update);
-        } else if (message.hasText() && message.hasEntities()) {
+        }else if(message.hasText() && message.hasEntities()){
             Optional<MessageEntity> commandEntity =
-                    message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
-            if (commandEntity.isPresent()) {
+                message.getEntities().stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
+            if(commandEntity.isPresent()){
                 String command = message.getText().substring((commandEntity.get().getOffset()), commandEntity.get().getLength());
-                switch (command) {
+                switch (command){
                     case "/start":
                         execute(SendMessage.builder().chatId(message.getChatId().toString()).text(START_MESSAGE).build());
                         creatingNewUser(update);
@@ -107,6 +123,7 @@ public class GamingBot extends TelegramLongPollingBot {
                                         "\n" +
                                         "Предсказывай дальше и получай ещё больше :)").
                                 build());
+
                         break;
                 }
             }
@@ -124,8 +141,8 @@ public class GamingBot extends TelegramLongPollingBot {
                     session.createCriteria(Person.class).add(
                             Restrictions.eq("answer", actionAnswer)
                     ).list();
-            for (Person person : list) {
-                person.setPoints(person.getPoints() + 250);
+            for(Person person : list){
+                person.setPoints(person.getPoints()+250);
                 execute(SendMessage.builder().chatId(person.getChat_id()).text("А вот и ставочка сыграла! + 250").build());
             }
 
@@ -136,15 +153,15 @@ public class GamingBot extends TelegramLongPollingBot {
             TypedQuery allQuery = session.createQuery(all);
             List<Person> listForClear = allQuery.getResultList();
 
-            for (Person person : listForClear)
+            for(Person person : listForClear)
                 person.setAnswer(null);
             session.getTransaction().commit();
-        } finally {
+        }finally {
             session.close();
         }
     }
 
-    public void creatingNewUser(Update update) {
+    public void creatingNewUser(Update update){
         Session session = sessionFactory.openSession();
         Message message = update.getMessage();
         try {
@@ -152,12 +169,12 @@ public class GamingBot extends TelegramLongPollingBot {
             Person person = new Person(message.getChatId().toString(), 0, null);
             session.save(person);
             session.getTransaction().commit();
-        } finally {
+        }finally {
             session.close();
         }
     }
 
-    public Person getPersonFromDataBase(Update update) {
+    public Person getPersonFromDataBase(Update update){
         Message message = update.getMessage();
         Session session = sessionFactory.openSession();
         try {
@@ -170,13 +187,12 @@ public class GamingBot extends TelegramLongPollingBot {
             Person person = (Person) list.get(0);
             session.getTransaction().commit();
             return person;
-        } finally {
+        }finally {
             session.close();
         }
     }
-
     @SneakyThrows
-    public void createAdminPost(Update update) {
+    public void createAdminPost(Update update){
         Message message = update.getMessage();
         String[] splitTextFromMessage = message.getText().split("-");
         firstCommand = splitTextFromMessage[0];
@@ -185,7 +201,7 @@ public class GamingBot extends TelegramLongPollingBot {
                 (Arrays.asList(InlineKeyboardButton.builder().text(firstCommand).callbackData("ORIGINAL:").build(),
                         InlineKeyboardButton.builder().text(secondCommand).callbackData("TARGET:").build()));
         List<Person> list = findAllPeopleWithCriteria();
-        for (Person person : list) {
+        for(Person person : list){
             execute(SendMessage.
                     builder().
                     chatId(person.getChat_id()).
@@ -215,13 +231,13 @@ public class GamingBot extends TelegramLongPollingBot {
 
             TypedQuery allQuery = session.createQuery(all);
             return allQuery.getResultList();
-        } finally {
+        }finally {
             session.close();
         }
     }
 
     @SneakyThrows
-    public void handingButton(Update update) {
+    public void handingButton(Update update){
         Message callbackMessage = update.getCallbackQuery().getMessage();
         String[] param = update.getCallbackQuery().getData().split(":");
         String action = param[0];
@@ -235,14 +251,14 @@ public class GamingBot extends TelegramLongPollingBot {
                             Restrictions.eq("chat_id", chatTdFromMessage)
                     ).list();
             Person person = (Person) list.get(0);
-            if (person.getAnswer() == null) {
+            if(person.getAnswer() == null && sendGolos == true){
                 execute(SendMessage.builder().chatId(person.getChat_id()).text("▫️Ваше предсказание принято☑️").build());
                 person.setAnswer(action);
-            } else {
+            }else{
                 execute(SendMessage.builder().chatId(person.getChat_id()).text("Жизнь не дает второго шанса").build());
             }
             session.getTransaction().commit();
-        } finally {
+        }finally {
             session.close();
         }
     }
@@ -251,12 +267,53 @@ public class GamingBot extends TelegramLongPollingBot {
         try {
             Double.parseDouble(str);
             return true;
-        } catch (NumberFormatException e) {
+        } catch(NumberFormatException e){
             return false;
         }
     }
-
-    public void hadingOnDelete() {
-
-    }
 }
+
+/*
+Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        try
+        {
+            session.beginTransaction();
+            String chat_id = "2";
+            Query query = session.createQuery("from Person where chat_id=chat_id");
+            Person person = (Person)query.stream().toArray()[0];
+            session.delete(person);
+            session.getTransaction().commit();
+        }finally {
+            session.close();
+            sessionFactory.close();
+        }
+ */
+
+
+/*
+ @SneakyThrows
+    public void handingButton(Update update){
+        Message callbackMessage = update.getCallbackQuery().getMessage();
+        String[] param = update.getCallbackQuery().getData().split(":");
+        String action = param[0];
+        System.out.println(action);
+        Session session = sessionFactory.openSession();
+        try {
+            String chatTdFromMessage = callbackMessage.getChatId().toString();
+            session.beginTransaction();
+            List list =
+                    session.createCriteria(Person.class).add(
+                            Restrictions.eq("chat_id", chatTdFromMessage)
+                    ).list();
+            Person person = (Person) list.get(0);
+            if(person.getAnswer() == null){
+                person.setAnswer(action);
+            }
+            session.getTransaction().commit();
+        }finally {
+            session.close();
+        }
+    }
+ */
